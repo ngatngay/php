b8324aaccdf6287c1441979b47306ccace3aa6ec
ngatngay <ngatngay@ngatngay.net>
2025-03-02 08:12:28 +0700

    update

---
+++ src/zip.php
@@ -0,0 +1,26 @@
+<?php
+
+class zip extends ZipArchive {
+    public function add($path, $relative = null)
+    {
+        if (!file_exists($path)) {
+            return false;
+        }
+        
+        $file = new SplFileInfo($path);
+        $path = $file->getPathname();
+        $pathRelative = $path;
+
+        if ($relative) {
+            $pathRelative = substr($path, strlen($relative));
+        }
+    
+        if ($file->isFile()) {
+            $this->addFile($path, $pathRelative);
+        }
+        
+        if ($file->isDir()) {
+            $this->addEmptyDir($pathRelative);
+        }
+    }
+}
